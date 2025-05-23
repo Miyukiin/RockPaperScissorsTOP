3a9b55229bb2f96ff2afe7853ea100ba3f0d6869
Miyukiin <jr.concha99@gmail.com>
2025-05-23 17:08:35 +0800

    Add playRound function

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ Pseudocode:
 
 */
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let randomNumber = (Math.random().toFixed(2)) * 100;
     let computerChoice = null;
@@ -23,3 +26,64 @@ function getHumanChoice(){
     let humanChoice = prompt("Enter your choice.").trim().toLowerCase();
     return humanChoice;
 }
+
+function postRoundMessage(outcome, humanChoice, computerChoice){
+    if(outcome == "win"){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        return;
+    }
+    else if(outcome == "lose"){
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        return;
+    }
+    console.log(`It's a draw! Both chose ${humanChoice}!`)
+
+
+}
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice == "rock"){
+        if (computerChoice == "scissors"){
+            humanScore++;
+            postRoundMessage("win", humanChoice, computerChoice);
+            return;
+        }
+        else if (computerChoice=="paper"){
+            computerScore++;
+            postRoundMessage("lose", humanChoice, computerChoice);
+            return;
+        }
+    }
+    else if (humanChoice == "paper"){
+        if (computerChoice == "rock"){
+            humanScore++;
+            postRoundMessage("win", humanChoice, computerChoice);
+            return;
+        }
+        else if (computerChoice=="scissors"){
+            computerScore++;
+            postRoundMessage("lose", humanChoice, computerChoice);
+            return;
+        }
+    }
+    else if (humanChoice == "scissors"){
+        if (computerChoice == "paper"){
+            humanScore++
+            postRoundMessage("win", humanChoice, computerChoice);
+            return;
+        }
+        else if (computerChoice=="rock"){
+            computerScore++;
+            postRoundMessage("lose", humanChoice, computerChoice);
+            return;
+        }
+    }
+    postRoundMessage("draw", humanChoice, computerChoice);
+
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
